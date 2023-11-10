94caf9f4b82d65fd4bc33d70d60f9c1905b216b8
yongho9064 <dydgmldydgh@naver.com>
2023-11-10 14:31:31 +0900

    :tada: new project

---
+++ src/main/java/com/yongho/bookvillage/BookvillageApplication.java
@@ -0,0 +1,11 @@
+package com.yongho.bookvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvillageApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BookvillageApplication.class, args);
+	}
+}
---
+++ src/test/java/com/yongho/bookvillage/BookvillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yongho.bookvillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookvillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
